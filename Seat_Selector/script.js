6c7e12519b111b8433d74f124aaa76415266bffa
BIRENDRA OLI <dev.birendraoli@gmail.com>
2023-12-04 22:50:00 +0545

    script updated

--- Seat_Selector/script.js
+++ Seat_Selector/script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-        const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+        const seats = document.querySelectorAll(".row .seat:not(.booked)");
         const count = document.getElementById("count");
         const total = document.getElementById("total");
         const movieSelect = document.getElementById("movie");
@@ -32,7 +32,7 @@ const container = document.querySelector(".container");
         container.addEventListener("click", (e) => {
             if (
                 e.target.classList.contains("seat") &&
-                !e.target.classList.contains("occupied")
+                !e.target.classList.contains("booked")
             ) {
                 e.target.classList.toggle("selected");
 
